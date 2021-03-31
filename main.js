5b61da495d932e92dd1806530abfe0b508d9e7ea
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-03-31 04:19:25 +0200

    feat: added links to li nav tags

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const parent__nav = Array.from(document.querySelectorAll(".parent__list__item"))
-console.log(parent__nav)
-parent__nav.forEach(parent => {
-    parent.addEventListener("click", () => {
+const parent__btn = Array.from(document.querySelectorAll(".parent__list__item__btn"))
+console.log(parent__btn)
+parent__btn.forEach(btn => {
+    btn.addEventListener("click", () => {
         console.log("parent has been clicked")
-        parent.lastElementChild.classList.toggle("show__nav__list__child")
-        parent.firstElementChild.classList.toggle("rotate__nav__first__child")
-        console.log(parent.firstElementChild)
+        btn.nextElementSibling.classList.toggle("show__nav__list__child")
+        btn.firstElementChild.classList.toggle("rotate__nav__first__child")
+        // console.log(parent.firstElementChild)
     })
 })
