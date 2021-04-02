2571eda9cb326129d2fbee6de450bc518a0ad4c3
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-04-02 10:36:32 +0200

    header section -hover on link tags

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const parent__btn = Array.from(document.querySelectorAll(".parent__list__item--btn"))
-console.log(parent__btn)
+const open__menu = document.querySelector(".open__menu")
+const close__menu = document.querySelector(".close__menu")
+const nav__list__container =  document.querySelector(".nav__list__container")
+console.log(nav__list__container)
+
+open__menu.addEventListener("click", () => {
+    close__menu.classList.toggle("js__close__menu")
+    open__menu.classList.toggle("js__open__menu")
+    nav__list__container.classList.toggle("show__nav__list__container")
+})
+
+close__menu.addEventListener("click", () => {
+    close__menu.classList.toggle("js__close__menu")
+    open__menu.classList.toggle("js__open__menu")
+    nav__list__container.classList.toggle("show__nav__list__container")
+})
+
 parent__btn.forEach(btn => {
     btn.addEventListener("click", () => {
         console.log("parent has been clicked")
